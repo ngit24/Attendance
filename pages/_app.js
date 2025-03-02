bc0e9dfb285f6494596d68793f78d381e8cd30b0
ngit24 <userrr2203@proton.me>
2025-03-02 17:31:40 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,8 @@ import '../styles/index.css';
 import '../styles/greeting.css';
 import '../styles/neo-day.css';
 import '../styles/sub.css';
+import '../styles/popup.css';
+
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
